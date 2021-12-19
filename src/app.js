33dbaddb0b85d303028d239e30c1215c4c52f23a
AniaMoskal <annaa.moskal@gmail.com>
2021-12-19 12:58:44 +0100

    added js for additional information

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
-
 // showing current day of the week 
-
 let dayElement = document.querySelector("#day");
 let currentDay = new Date();
 let day = currentDay.getDay(); 
 let days = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
-
 dayElement.innerHTML = `${days[day]}`;
-
 // showing current time + added ) in front of minutes
-
 let timeElement = document.querySelector("#time");
 let currentTime = new Date();
 let hours = currentTime.getHours();
@@ -17,32 +12,33 @@ let minutes = currentTime.getMinutes();
 if (minutes < 10) {
   minutes = `0${minutes}`;
 }
-
 timeElement.innerHTML = `${hours}:${minutes}`;
-
 // displaying a name of the city after a user submts the form
-
-
-// funtion for the weather condition that we mention in the funtion below
+// funtion for the weather condition that we mention in the function below
 function displayWeatherCondition(response){
   console.log(response.data.name);
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#currenttemprature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#feelstemprature").innerHTML = Math.round(response.data.main.feels_like);
-  document.querySelector("#currentweather").innerHTML = response.data.weather[0].description;
+  document.querySelector("#currentweather").innerHTML = response.data.weather[0].main;
+  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#sunrise").innerHTML = response.data.sys.sunrise;
+  document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
+  
   let iconElement = document.querySelector("#bigicon");
-  iconElement.setAttribute("class", `wi wi-owm-${response.data.weather[0].id} bigicon`);
-  //iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+
+iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 // function for the onload search for Berlin
-
 function searchCity(city) {
   let apiKey = "4e13aa90127904d13b6a4af59475dbf3";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   // let cityElement = document.querySelector("#city");
@@ -55,33 +51,23 @@ function handleSubmit(event) {
   // let apiKey = "4e13aa90127904d13b6a4af59475dbf3";
   // let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   // axios.get(apiUrl).then(displayWeatherCondition);
-
   // calling for the search city function so search for other cities will work 
   searchCity(city);
 }
-
-
 let searchForm = document.querySelector("#search-form");
-
 searchForm.addEventListener("submit", handleSubmit);
-
 // API for current location
-
 function searchLocation(position) {
   let apiKey = "4e13aa90127904d13b6a4af59475dbf3";
-  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
 // funtion for button current submit 
-
 function getCurrentLocation (event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
-
 // changing C to F on a clik (with fake data)
-
 function convertToFahrenheit(event)
 {
   event.preventDefault();
@@ -89,12 +75,9 @@ function convertToFahrenheit(event)
   let currenttemprature = temperatureElement.innerHTML;
   temperatureElement.innerHTML = Math.round((currenttemprature * 9) / 5 + 32);
 }
-
 let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", convertToFahrenheit);
-
 // C - F
-
 function convertToFahrenheitSymbol(event)
 {
   event.preventDefault();
@@ -102,12 +85,9 @@ function convertToFahrenheitSymbol(event)
   let temperaturesymbol = temperatureSymbolElement.innerHTML;
   temperatureSymbolElement.innerHTML = "°F";
 }
-
 let fahrenheitsymbol = document.querySelector("#fahrenheit");
 fahrenheitsymbol.addEventListener("click", convertToFahrenheitSymbol);
-
 // changing F to C on a clik (with fake data)
-
 function convertToCelcius(event)
 {
   event.preventDefault();
@@ -115,12 +95,9 @@ function convertToCelcius(event)
   let currenttemprature = temperatureElement.innerHTML;
   temperatureElement.innerHTML = Math.round((currenttemprature - 32) * 5 / 9);
 }
-
 let celcius = document.querySelector("#celcius");
 celcius.addEventListener("click", convertToCelcius);
-
 // F - C
-
 function convertToCelciusSymbol(event)
 {
   event.preventDefault();
@@ -128,15 +105,10 @@ function convertToCelciusSymbol(event)
   let temperaturesymbol = temperatureSymbolElement.innerHTML;
   temperatureSymbolElement.innerHTML = "°C";
 }
-
 let celciussymbol = document.querySelector("#celcius");
 celciussymbol.addEventListener("click", convertToCelciusSymbol);
-
-
 // adding current location button 
-
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
-
 // calling a city on load 
 searchCity("Berlin");
